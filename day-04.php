656c3e9fee13115e3f70ba57049486f414ef9c6a
DjLeChuck <djlechuck@gmail.com>
2015-12-13 17:58:24 +0100

    Refactoring day 04

--- day-04.php
+++ day-04.php
@@ -4,22 +4,20 @@
  * Parts one and two are OK.
  */
 
-$data   = trim(file_get_contents('inputs/day-04.txt'));
-$number = 0;
-$hash   = null;
-
-do {
-    $hash = md5($data.++$number);
-} while (substr($hash, 0, 5) !== '00000');
-
-echo sprintf('First part: %u', $number).PHP_EOL;
-
-$number = 0;
+$data       = trim(file_get_contents('inputs/day-04.txt'));
+$number     = 0;
+$hash       = null;
+$number_one = null;
 
 do {
     set_time_limit(1);
 
     $hash = md5($data.++$number);
+
+    if (null === $number_one && substr($hash, 0, 5) === '00000') {
+        $number_one = $number;
+    }
 } while (substr($hash, 0, 6) !== '000000');
 
+echo sprintf('First part: %u', $number_one).PHP_EOL;
 echo sprintf('Second part: %u', $number).PHP_EOL;
