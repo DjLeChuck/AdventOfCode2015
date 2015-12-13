bdd50a74eee777e15be9b1edcab0070e1d7d7d12
DjLeChuck <djlechuck@gmail.com>
2015-12-13 10:35:59 +0100

    Clean code
    - Move inputs in externals files
    - Clean some code

--- day-04.php
+++ day-04.php
@@ -1,26 +1,25 @@
 <?php
 
-$secret = 'ckczppom';
+/*
+ * Parts one and two are OK.
+ */
+
+$data   = trim(file_get_contents('inputs/day-04.txt'));
 $number = 0;
-$hash = null;
+$hash   = null;
 
 do {
-	$number++;
-	$hash = md5($secret.$number);
+    $hash = md5($data.++$number);
 } while (substr($hash, 0, 5) !== '00000');
 
-echo $number;
-
-echo '<br><br>';
+echo sprintf('First part: %u', $number).PHP_EOL;
 
 $number = 0;
 
 do {
-	set_time_limit(1);
-	$number++;
-	$hash = md5($secret.$number);
+    set_time_limit(1);
+
+    $hash = md5($data.++$number);
 } while (substr($hash, 0, 6) !== '000000');
 
-echo $number;
-
-echo '<br><br>done';
+echo sprintf('Second part: %u', $number).PHP_EOL;
