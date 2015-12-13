54ed850d09fc4f003832d99608c5f1faf3695b19
DjLeChuck <djlechuck@gmail.com>
2015-12-13 18:43:28 +0100

    Remove useless code day 08

--- day-08.php
+++ day-08.php
@@ -4,14 +4,12 @@
  * Only part one is OK.
  */
 
-$data       = trim(file_get_contents('inputs/day-08.txt'));
-$part_one   = $data;
-$part_two   = explode("\n", $data);
-$a          = 0;
-$b          = 0;
+$data   = trim(file_get_contents('inputs/day-08.txt'));
+$a      = 0;
+$b      = 0;
 
-foreach (explode("\n", $part_one) as $key => $line) {
-    $linebis    = substr($part_two[$key], 1);
+foreach (explode("\n", $data) as $key => $line) {
+    $linebis    = substr($line, 1);
     $linebis    = substr($linebis, 0, -1);
     $linebis    = str_replace(['\"', '\\\\'], '"', $linebis);
     $linebis    = preg_replace('`(\\\\x[a-f0-9]{2})`', 'x', $linebis);
